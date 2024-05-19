93943cc70fcd2180b1324fba0ec3d6da4a9b1eeb
Connor Warme <connor.warme@gmail.com>
2024-05-18 20:45:13 -0600

    Figure out how to seed database

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,24 +1,29 @@
-import { db } from '@vercel/postgres';
+"use server"
+import 'dotenv/config';
+import { sql } from '@vercel/postgres';
 
 const users = [
   { name: 'John Doe', email: 'jd@mail.com'},
   { name: 'Jane Doe', email: 'jad@mail.com'},
   { name: 'James Doe', email: 'jdd@mail.com'},
 ]
-async function seedUsers(client) {
+async function seedUsers() {
+
+  console.log(process.env.POSTGRES_URL)
   try {
-    const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS users (
-      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    const getUsers = await sql`SELECT * FROM users;`;
+    const createTable = await sql`
+    CREATE TABLE people (
+      id UUID DEFAULT gen_random_uuid(),
       name VARCHAR(255) NOT NULL,
-      email TEXT NOT NULL UNIQUE,
+      email VARCHAR(255) NOT NULL UNIQUE
     );`;
-    console.log('create user table');
+    console.log(getUsers);
 
     const insertedUsers = await Promise.all(
       users.map(async (user) => {
-        const result = await client.sql`
-        INSERT INTO users (name, email)
+        const result = await sql`
+        INSERT INTO people (name, email)
         VALUES (${user.name}, ${user.email})
         RETURNING *;`;
         return result;
@@ -31,9 +36,7 @@ async function seedUsers(client) {
   }
 }
 export async function main() {
-  const client = await db.connect();
-  await seedUsers(client)
-  await client.end();
+  await seedUsers()
 }
 main().catch((error) => {
   console.error('Error connecting to database:', error);
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import Image from "next/image";
 
 export default function Home() {
