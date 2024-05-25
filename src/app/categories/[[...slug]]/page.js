51d6583f519ac6082b22f3274d45dacf15b8955a
Connor Warme <connor.warme@gmail.com>
2024-05-25 17:37:35 -0600

    Add notes for future self

--- src/app/categories/[[...slug]]/page.js
+++ src/app/categories/[[...slug]]/page.js
@@ -2,6 +2,8 @@ export default function Categories({ params }) {
   // params.slug is an array of the slug parts
   if (params.slug?.length === 2) {
     // w/ a length of 2, we have a category and a product
+    // e.g. /categories/shirts/sunhoody
+    // would need to query for product (details) and category (name only? url?)
     return (
       <div>
         <h3>Category: {params.slug[0]}</h3>
@@ -10,6 +12,7 @@ export default function Categories({ params }) {
     )
   } else if (params.slug?.length === 1) {
     // w/ a length of 1, we have a category
+    // need to query for items in that category
     return (
       <div>
         <h3>Category: {params.slug[0]}</h3>
