138a7d7e8aded0720698ebcfc05ef5ae178da09f
Connor Warme <connor.warme@gmail.com>
2024-06-15 17:06:14 -0600

    Learn about active links via pathname

--- src/app/ui/nav.jsx
+++ src/app/ui/nav.jsx
@@ -1,18 +1,29 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Nav() {
+  const navLinks = [
+    { href: "/dashboard", label: "Dashboard" },
+    { href: "/dashboard/orders", label: "Orders" },
+    { href: "/products", label: "Products" },
+    { href: "/categories", label: "Categories" },
+  ]
+  const pathname = usePathname();
+  
+  const linkStyle = "text-blue-500 hover:text-blue-700 p-2";
+  const linkStyleActive = "text-blue-700 bg-white border border-blue-700 p-2";
   return (
     <nav>
       <ul>
-        <li>
-          <Link href="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/dashboard/orders">Orders</Link>
-        </li>
-        <li>
-          <Link href="/products">Products</Link>
-        </li>
+        { navLinks.map(({ href, label }) => (
+          <li key={href} className="m-2">
+            <Link href={href} className={pathname.endsWith(href) ? linkStyleActive : linkStyle}>
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
