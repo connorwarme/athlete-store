26303478d704408cecf0c28c33299daa0c5f511d
Connor Warme <connor.warme@gmail.com>
2024-07-26 20:39:16 -0600

    Add animation to img slide effect

--- src/app/components/carouselPhoto.jsx
+++ src/app/components/carouselPhoto.jsx
@@ -20,10 +20,16 @@ const CarouselPhoto = () => {
 
   const buttonDivStyle = 'flex absolute top-0 bottom-0 p-4 bg-black bg-opacity-0 hover:bg-opacity-25 transition-all duration-100 ease-in-out group';
   const buttonStyle = 'self-center px-4 text-lg font-bold text-white group-hover:animate-bounce';
+  const imgStyle = 'w-full h-full object-cover block grow-0 shrink-0 transition-all duration-300 ease-in-out';
   return (
     <div className='max-w-[1200px] w-full aspect-[10/6] my-auto mx-0'>
-      <div className='w-full h-full relative'>
-        <img src={images[imageIndex]} alt='carousel' className='w-full h-full object-cover block' />
+      <div className='w-full h-full relative overflow-hidden flex'>
+        {
+          images.map((img, index) => (
+            <img key={index} src={img} className={imgStyle} style={{ translate: `${-100 * imageIndex}%` }}/>
+          ))
+        }
+        {/* <img src={images[imageIndex]} alt='carousel' className='w-full h-full object-cover block' /> */}
         <div className={buttonDivStyle + ' right-0'}>
           <button onClick={handleForward} className={buttonStyle}>&gt;</button>
         </div>
