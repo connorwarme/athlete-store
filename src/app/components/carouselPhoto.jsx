5323b2b3a441b45bc0a25b2aae9307fa7a6e2d0a
Connor Warme <connor.warme@gmail.com>
2024-08-19 12:09:42 -0600

    Improve access w/ labels, hiding extra content, etc

--- src/app/components/carouselPhoto.jsx
+++ src/app/components/carouselPhoto.jsx
@@ -9,7 +9,13 @@ import { useState } from "react";
 
 const CarouselPhoto = () => {
   const [imageIndex, setImageIndex] = useState(0);
-  const images = [pic1.src, pic2.src, pic3.src, pic4.src, pic5.src];
+  const images = [
+    { url: pic1.src, alt: "Amity on Golden Gate" }, 
+    { url: pic2.src, alt: "Amity on Tales of Power" },
+    { url: pic3.src, alt: "Amity on Book of Hate" },
+    { url: pic4.src, alt: "Amity on Barnacle Scars" },
+    { url: pic5.src, alt: "Amity on Tainted Love" }
+  ];
 
   const handleBack = () => {
     return setImageIndex((prevIndex) =>
@@ -29,21 +35,22 @@ const CarouselPhoto = () => {
   const imgStyle =
     "w-full h-full object-cover block grow-0 shrink-0 transition-all duration-300 ease-in-out";
   return (
-    <div className="max-w-[1200px] w-full aspect-[10/6] my-auto mx-0">
+    <section className="max-w-[1200px] w-full aspect-[10/6] my-auto mx-0" aria-label="Image Slider">
       <div className="w-full h-full relative overflow-hidden flex">
         {images.map((img, index) => (
           <img
             key={index}
-            src={img}
+            src={img.url}
+            alt={img.alt}
+            aria-hidden={index === imageIndex ? "false" : "true"}
             className={imgStyle}
             style={{ translate: `${-100 * imageIndex}%` }}
           />
         ))}
-        {/* <img src={images[imageIndex]} alt='carousel' className='w-full h-full object-cover block' /> */}
-        <button onClick={handleForward} className={buttonDivStyle + " right-0"}>
+        <button onClick={handleForward} className={buttonDivStyle + " right-0"} aria-label="View Next Image">
             <span className={buttonStyle}>&gt;</span>
         </button>
-        <button onClick={handleBack} className={buttonDivStyle + " left-0"}>
+        <button onClick={handleBack} className={buttonDivStyle + " left-0"} aria-label="View Previous Image">
           <span className={buttonStyle}>
             &lt;
           </span>
@@ -54,13 +61,14 @@ const CarouselPhoto = () => {
           <button
             key={index}
             onClick={() => setImageIndex(index)}
-            className="w-8 h-8 border border-red-500 rounded-full bg-opacity-20 hover:bg-slate-700 transition-all duration-100 ease-in-out hover:scale-125 focus-visible:outline outline-red-500"
+            aria-label={`View Image ${index + 1}`}
+            className="w-8 h-8 border border-red-500 rounded-full bg-opacity-80 bg-slate-700 transition-all duration-100 ease-in-out hover:scale-125 focus-visible:outline outline-red-500 text-white font-xl flex justify-center items-center"
           >
             {index === imageIndex ? "🔴" : "⚪"}
           </button>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
